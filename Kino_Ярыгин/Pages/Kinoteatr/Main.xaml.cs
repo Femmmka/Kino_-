418fe61f562e26d45e12048c8c47b87aa1916e59
Ефим <fimakrut@mail.ru>
2024-03-16 13:23:42 +0500

    Созданный Кинотеатр. Ярыгин

---
+++ Kino_Ярыгин/Pages/Kinoteatr/Main.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Kino_Ярыгин.Classes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kino_Ярыгин.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        List<KinoteatrContext> AllKinoteatrs = KinoteatrContext.Select();
+        public Main()
+        {
+            InitializeComponent();
+            foreach (KinoteatrContext item in AllKinoteatrs)
+            {
+                parent.Children.Add(new Items.Item(item, this));
+            }
+        }
+
+        private void AddRecord(object sender, RoutedEventArgs e) =>
+            MainWindow.init.OpenPage(new Pages.Kinoteatr.Add());
+    }
+}
